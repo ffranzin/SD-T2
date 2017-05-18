a916e73b1a34651a54d99eccfd5c9a782c25cd05
ffranzin email = ffranzin@inf.ufsm.br <ffranzin@inf.ufsm.br>
2017-05-17 22:58:46 -0300

    asd

--- src/UserChat.java
+++ src/UserChat.java
@@ -1,11 +1,24 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 public class UserChat implements IUserChat{
 	
+	private String nameUser;
 	
+	public UserChat(String nameUser){
+		this.nameUser = nameUser;
+		try {
+			ServerRoomChat server = (ServerRoomChat) Naming.lookup("//127.0.0.1:1099/ServerRoom");
+		} catch (Exception e){
+			
+		}
+	}
+	
+	// para recebimento de mensagens
 	public void deliverMsg(String senderName, String msg)
 	{
-		
-		
 		
 	}
 	
